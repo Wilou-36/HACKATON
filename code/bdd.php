32f1bc8cb8120fd53d4bccc5b03e28a811a9efce
will <williams.dorego@lan.sio.lyceefulbert.fr>
2025-05-22 14:00:37 +0200

    'com'

--- code/bdd.php
+++ code/bdd.php
@@ -1,7 +1,7 @@
 <?php
     class BDD {
-        private $password = 'noel';
-        private $username = 'noel';
+        private $password = 'papnoel';
+        private $username = 'papnoel';
         private $servername = '192.168.10.20';
         private $dbname = 'Noel';
         private $port= 3306;
