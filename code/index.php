f6d0f0c3c58fe9feed5e09977220c688ede8597d
Your Namewill <williams.dorego@lan.sio.lyceefulbert.fr>
2025-05-22 11:40:09 +0200

    add all

--- code/index.php
+++ code/index.php
@@ -1,4 +1,20 @@
 <?php
-include('ap.php');
+    include('ap.php');
 
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Papa noel</title>
+        <link rel="icon" href="nonoel.png" type="image/png">
+    
+    </head>
+    <body>
+       <header> 
+       <h1> P'tit papa noel</h1>
+       </header>
+
+    </body>
 
